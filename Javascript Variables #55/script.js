c0e58041a401fc1f30af89b3f954ce4580d901a4
Sandesh Upadhyay <upadhyaysandesh123@gmail.com>
2024-04-08 11:05:18 +0530

    Js  Variables file added

---
+++ Javascript Variables #55/script.js
@@ -0,0 +1,92 @@
+// console.log("Hello World", "How are You JavaScript")
+// var a = 6;
+// var b = 9;
+// let c = 11;
+// console.log(a + b + 10 + c);
+
+
+// var a= 5;
+// var b = 10;
+// console.log (a+b);
+
+// let a = 2;
+// let b = 15;
+// console.log(a + b)
+
+
+// let a = 20;
+// var a = 20;
+// console.log(a)
+// console.log(a)
+
+// {
+//     var a = 20;
+//     let a= 20;
+//     console.log(a)
+// }
+//     console.log(a)
+
+
+//     var a = 20;
+//     var b = 50;
+//     let c=  a + b;
+//     console.log(c)
+
+//     let x = 70;
+//     var y = 30;
+    
+//     let z= x+y;
+//     console.log(z)
+
+//     {
+//     let x= 200;
+//     var y = 100;
+//     var z = x+y;
+//     consol.log(z)
+//     }
+
+
+// let a = 10;
+// {
+//     let a = 10;
+//     let b = 30;
+//     console.log(a+b);
+// }
+
+// const b = 60;
+// {
+//     const b = 60;
+//     const c = 70;
+//     // const b = 60;
+//     console.log(b - c);
+// }
+
+// var x = 10;
+// {
+//     var x = 10;
+//     var y = 20;
+//     // var x = 30;
+//     console.log(x * y);
+// }
+
+// let a = 30;
+// {
+//     let a = 30;
+//     let b = 20;
+//     console.log(a / b);
+// }
+
+
+let x = 16 + "Jaguar";
+console.log(x)
+
+let y = "16" + "Jaguar"
+console.log(y)
+
+
+let a = 123e5;
+let z = 123e-5;
+console.log(a ,z)
+
+let a1 = BigInt(12345678901234567890123456789);
+console.log(a1)
