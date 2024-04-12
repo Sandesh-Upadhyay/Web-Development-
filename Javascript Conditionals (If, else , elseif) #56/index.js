c70784408888e3791d3771264f895299fb9ea6fd
Sandesh Upadhyay <upadhyaysandesh123@gmail.com>
2024-04-12 21:09:42 +0530

    Js  conditions file added

---
+++ Javascript Conditionals (If, else , elseif) #56/index.js
@@ -0,0 +1,30 @@
+console.log("Hello Javascript");
+let x = 32;
+if (x <= 24){
+    console.log("No You are not my relative")
+}
+else{
+    console.log("you are my relative ")
+}
+let a = 55;
+if (a <= 55){
+    console.log("Equal")
+}
+else{
+    console.log("Not Equal")
+}
+
+let hour = 48;
+if (hour != 68){
+    console.log("Not Sleeping Well")
+}
+// else if(hour == 24){
+//     console.log("Slept")
+// }
+else if( hour === 48){
+    console.log("type is...")
+}
+else{
+    console.log("Sleeping Well")
+}
+
