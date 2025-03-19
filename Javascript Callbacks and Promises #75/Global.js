b45bca26edfb159b7e7b5d6a86e50ed8ccd1fd8d
Sandesh-Upadhyay <7523sandeshupadhyay@gmail.com>
2025-03-19 22:28:17 +0530

    update files with node examples

--- Javascript Callbacks and Promises #75/Global.js
+++ Javascript Callbacks and Promises #75/Global.js
@@ -1,2 +1,17 @@
-let Global = 'Global';
-console.log(Global);
+// Description: This file contains the code to demonstrate the global object in Node.js.
+// Date: 19th March, 2025
+
+// console.log(global);
+
+// global.setTimeout(() => {   
+//     console.log("Hello World");
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log("Hello World");
+// }, 1000);
+
+
+console.log(__dirname);
+console.log(__filename);
